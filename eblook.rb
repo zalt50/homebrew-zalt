ce34ac3d679a244346726566bf2cebcf28180874
Takahiro SATOH <zalt50cc@gmail.com>
2016-01-09 14:49:04 +0900

    Update eblook

--- eblook.rb
+++ eblook.rb
@@ -1,12 +1,12 @@
-require 'formula'
+require "formula"
 
 class Eblook < Formula
-  url 'http://ikazuhiro.s206.xrea.com/filemgmt_data/files/eblook-1.6.1%2Bmedia-20130919.tar.gz'
-  homepage 'http://green.ribbon.to/~ikazuhiro/lookup/lookup.html#EBLOOK'
-  sha256 'd66fb9ac372d11f7b66f5019998ed02d86db0d44ce886f13542f9ee6bbe748f4'
-  version '1.6.1+media-20130919'
-  
-  depends_on 'eb'
+  url "http://green.ribbon.to/~ikazuhiro/lookup/files/eblook-1.6.1+media-20150724.tar.gz"
+  homepage "http://green.ribbon.to/~ikazuhiro/lookup/lookup.html#EBLOOK"
+  sha256 "08e154eae27cc061e0c67dc9c0c346707defbb2bbfdb757ee00f22af4c059e1b"
+  version "1.6.1+media-20150724"
+
+  depends_on "eb"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
