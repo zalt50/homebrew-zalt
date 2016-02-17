c31353a7aee8a1bb8add3d0fb4fed120dcba6e81
Takahiro SATOH <zalt50cc@gmail.com>
2016-02-17 10:19:39 +0900

    Follow the style guide

--- dcl-c.rb
+++ dcl-c.rb
@@ -1,12 +1,12 @@
-require 'formula'
+require "formula"
 
 class DclC < Formula
-  url 'http://gfd-dennou.org/library/dcl/dcl-6.0.4-C.tar.gz'
-  homepage 'http://gfd-dennou.org/index.html.en'
-  sha256 '62003ae11c4d4a88e5112d7055d5abdce71cc9cb274b823b77053aca2b486ea1'
-  version '6.0.4'
+  url "http://gfd-dennou.org/library/dcl/dcl-6.0.4-C.tar.gz"
+  homepage "http://gfd-dennou.org/index.html.en"
+  sha256 "62003ae11c4d4a88e5112d7055d5abdce71cc9cb274b823b77053aca2b486ea1"
+  version "6.0.4"
 
-  depends_on 'gtk+'
+  depends_on "gtk+"
 
   def install
     ENV.deparallelize
--- g77.rb
+++ g77.rb
@@ -1,4 +1,4 @@
-require 'formula'
+require "formula"
 
 class StandardHomebrewLocation < Requirement
   def message; <<-EOS.undent
@@ -8,16 +8,17 @@ class StandardHomebrewLocation < Requirement
     Homebrew is installed somewhere other than /usr/local
     EOS
   end
+
   def satisfied?
     HOMEBREW_PREFIX.to_s == "/usr/local"
   end
 end
 
 class G77 < Formula
-  homepage 'http://hpc.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/hpc/hpc/g77/g77-intel-bin.tar.gz'
-  version '3.4'
-  sha1 '29c3846b91f84b8bf6d7253e8ebf944ee8de08f3'
+  homepage "http://hpc.sourceforge.net"
+  url "http://downloads.sourceforge.net/project/hpc/hpc/g77/g77-intel-bin.tar.gz"
+  version "3.4"
+  sha1 "29c3846b91f84b8bf6d7253e8ebf944ee8de08f3"
 
   depends_on StandardHomebrewLocation.new
 
