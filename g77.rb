7b2a8e0a42ce8fafe813589e8333ea3ee123b81e
Takahiro SATOH <zalt50cc@gmail.com>
2018-10-09 13:01:29 +0900

    use sha256

--- g77.rb
+++ g77.rb
@@ -18,7 +18,7 @@ class G77 < Formula
   homepage "http://hpc.sourceforge.net"
   url "http://downloads.sourceforge.net/project/hpc/hpc/g77/g77-intel-bin.tar.gz"
   version "3.4"
-  sha1 "29c3846b91f84b8bf6d7253e8ebf944ee8de08f3"
+  sha256 "d29b45f2b2f374aa12cad202a305acd1453425ec86eeb52b05bbbbfe497ee427"
 
   depends_on StandardHomebrewLocation.new
 
