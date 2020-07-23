e48eb127a4898845af77c6a32fcb44530f254b97
Takahiro SATOH <zalt50cc@gmail.com>
2020-07-23 10:30:13 +0900

    use X11

---
+++ gnuplot.rb
@@ -0,0 +1,54 @@
+class Gnuplot < Formula
+  desc "Command-driven, interactive function plotting"
+  homepage "http://www.gnuplot.info/"
+  url "https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.4.0/gnuplot-5.4.0.tar.gz"
+  sha256 "eb4082f03a399fd1e9e2b380cf7a4f785e77023d8dcc7e17570c1b5570a49c47"
+  license "gnuplot"
+
+  head do
+    url "https://git.code.sf.net/p/gnuplot/gnuplot-main.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gd"
+  depends_on "libcerf"
+  depends_on "lua"
+  depends_on "pango"
+  depends_on "qt"
+  depends_on "readline"
+  depends_on :x11
+
+  def install
+    # Qt5 requires c++11 (and the other backends do not care)
+    ENV.cxx11
+
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-readline=#{Formula["readline"].opt_prefix}
+      --without-tutorial
+      --disable-wxwidgets
+      --with-qt
+    ]
+
+    system "./prepare" if build.head?
+    system "./configure", *args
+    ENV.deparallelize # or else emacs tries to edit the same file with two threads
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/gnuplot", "-e", <<~EOS
+      set terminal dumb;
+      set output "#{testpath}/graph.txt";
+      plot sin(x);
+    EOS
+    assert_predicate testpath/"graph.txt", :exist?
+  end
+end
