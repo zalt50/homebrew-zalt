19222bca097a9936c4024a52523f062e1b385b63
Takahiro SATOH <zalt50cc@gmail.com>
2020-11-03 16:00:22 +0900

    fix url

--- ebu.rb
+++ ebu.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Ebu < Formula
-  url "http://green.ribbon.to/~ikazuhiro/dic/files/ebu-4.4.3-20150301.tar.gz"
+  url "http://green.ribbon.to/~kazuhiro/dic/files/ebu-4.4.3-20150301.tar.gz"
   homepage "http://green.ribbon.to/~ikazuhiro/dic/ebu.html"
   sha256 "207fef55ebf1240b411dc3dd5dca01fb7b953e9f8b4b95d2e432e821db5e9fcc"
   version "4.4.3-20150301"
